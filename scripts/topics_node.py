10294250432fc3135bf427a6536a7e5f8bb0dee1
Mauricio Rosas <mark_mau_@hotmail.com>
2025-05-06 06:21:56 +0000

    Turtlebot working well

--- scripts/topics_node.py
+++ scripts/topics_node.py
@@ -1,31 +1,47 @@
 #! /usr/bin/env python
 
 import rospy
-import time
-from section2.srv import FindWall, FindWallResponse
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from section2.srv import FindWall, FindWallRequest
+
+data = None
+rate = None
 
 def callback(msg):
     global data
     data = msg
+    
 
-def my_callback(request):
-    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+def moved():
+    global rate
+    rospy.init_node('topics_node', anonymous=True)
+    rospy.wait_for_service('/find_wall') # Wait for the service client /move_bb8_in_circle_custom to be running
+    findwall_service_client = rospy.ServiceProxy('/find_wall', FindWall) # Create the connection to the service
+    findwall_request_object = FindWallRequest() # Create an object of type EmptyRequest
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     sub = rospy.Subscriber('/scan', LaserScan, callback)
-    move = Twist()
-    rate = rospy.Rate(1)
-    rate.sleep() #rospy.spin()  mantain the service open.
-    rospy.loginfo("The Service FindWall has been called")
+    rate = rospy.Rate(2)
+    rate.sleep()
     global data
+    print ("Running") 
     move = Twist()
     turn = 0
     X_Speed = 0.1
     x = 80
     t = 80
-    out = 0
-    
-    while not rospy.is_shutdown() and out == 0: 
+    out = 1
+
+    while out == 1:
+         result = findwall_service_client(findwall_request_object) # Send through the connection the path to the trajectory file to be executed
+         rospy.loginfo(str(result)) # Print the result given by the service called
+         if result.wallfound == True:
+              out = 0
+              result.wallfound = False 
+              
+    rospy.loginfo("END of Service call...")
+
+    while not rospy.is_shutdown(): 
          # values at 0 degree
          print ("Right ", data.ranges[0])
          # values at 90 degree
@@ -46,26 +62,22 @@ def my_callback(request):
                 turn = 0
                 if scan_right >= 0.3: #and scan_left == float('inf'):
                      turn = 0
-                     out = 1
                      move.angular.z = 0.0 #Move the with an angular velocity in the z axis
                      rate = rospy.Rate(x)
                      print ('Turn0')
                 elif scan_right >= 0.3: #and scan_left >= 0.2:
                      turn = 0
-                     out = 1
                      move.angular.z = 0.0 #Move the with an angular velocity in the z axis
                      rate = rospy.Rate(x)
                      print ('Turn0')                
          elif scan_middle >= 0.7 and turn == 1:          
                 if scan_right >= 0.3: #and scan_left == float('inf'):
                      turn = 0
-                     out = 1
                      move.angular.z = 0.0 #Move the with an angular velocity in the z axis
                      rate = rospy.Rate(x)
                      print ('Turn0')
                 elif scan_right >= 0.3: #and scan_left >= 0.2:
                      turn = 0
-                     out = 1
                      move.angular.z = 0.0 #Move the with an angular velocity in the z axis
                      rate = rospy.Rate(x)
                      print ('Turn0') 
@@ -171,29 +183,14 @@ def my_callback(request):
          # publish 
          pub.publish(move) 
          rate.sleep()
-         time.sleep(1)        
-                     
-    rospy.loginfo("Finished service find_wall")
-    move.linear.x = 0
-    move.angular.z = 0
-    pub.publish(move)
-    rate = rospy.Rate(1)
-    rate.sleep()
-    
-    response = FindWallResponse()
-    response.wallfound = True
-    return response # the service Response class, in this case EmptyResponse
 
-def main():
-  rospy.init_node('service_Find_Wall') 
-  my_service = rospy.Service('/find_wall', FindWall , my_callback) # create the Service called move_bb8_in_square_custom with the defined callback
-  rospy.loginfo("Service /find_wall Ready")
-  rate = rospy.Rate(1)
-  while not rospy.is_shutdown():
-       rate.sleep() #rospy.spin()  mantain the service open.
+
   
-main()
+        
+def main(): 
+  moved()
 
+  
 if __name__ == '__main__': 
     try: 
         main() 
